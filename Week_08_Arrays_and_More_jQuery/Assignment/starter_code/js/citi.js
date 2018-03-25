7cab99fb4a526b9c999d3cd3f8c4f19b307f1b93
Rose <rosellen.grant@gmail.com>
2018-03-25 12:15:02 -0400

    last changes

--- Week_08_Arrays_and_More_jQuery/Assignment/starter_code/js/citi.js
+++ Week_08_Arrays_and_More_jQuery/Assignment/starter_code/js/citi.js
@@ -1,38 +1,38 @@
 
+//create array of all possible cities
 var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
 
 
 
 jQuery(document).ready(function(){ 
 
-
-
+	//add list of options
 	var options = "";
     jQuery.each(cities, function(index, value) {
         options += '<option value="' + value + '">' + value + '</option>';
     });
-    jQuery("#city-type").append(options);
+    jQuery("#city-type").append(options); //add to HTML 
 
-    console.log(options);
 
+    //check which option is selected 
 	jQuery("#city-type").on('change',function() {
-	      if (jQuery("#city-type").val() == "NYC") {
+	      if (jQuery("#city-type").val() == "NYC") { //nyc
 	      	jQuery("body").attr("class","nyc");
 	      }
-	      else if (jQuery("#city-type").val() == "SF") {
+	      else if (jQuery("#city-type").val() == "SF") { //sf
 	      	jQuery("body").attr("class","sf");
 	      }
-	      else if (jQuery("#city-type").val() == "LA") {
+	      else if (jQuery("#city-type").val() == "LA") { //la
 	      	jQuery("body").attr("class","la");
 	      }
-	      else if (jQuery("#city-type").val() == "ATX") {
+	      else if (jQuery("#city-type").val() == "ATX") { //austin
 	      	jQuery("body").attr("class","austin");
 	      }
-	      else if (jQuery("#city-type").val() == "SYD") {
+	      else if (jQuery("#city-type").val() == "SYD") { //sydney
 	      	jQuery("body").attr("class","sydney");
 	      }
 	      else {
-	      	jQuery("body").attr("class","og");
+	      	jQuery("body").attr("class","og"); //base case
 	      }
 
 
